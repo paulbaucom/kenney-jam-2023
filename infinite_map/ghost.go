bbe75fa9dac4ffa5bbb86a14502ad4342106e4b3
Paul Baucom <paul.baucom@outlook.com>
2023-07-22 12:05:34 -0500

    Add light source and ghost script with idle anim

--- infinite_map/ghost.go
+++ infinite_map/ghost.go
@@ -1,3 +1,48 @@
+components {
+  id: "ghost"
+  component: "/main/ghost.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "lightsource"
+  component: "/lights/lightsource.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+  properties {
+    id: "color"
+    value: "0.7, 0.7, 0.7, 0.5"
+    type: PROPERTY_TYPE_VECTOR4
+  }
+  properties {
+    id: "radius"
+    value: "300.0"
+    type: PROPERTY_TYPE_NUMBER
+  }
+  properties {
+    id: "falloff"
+    value: "2.5"
+    type: PROPERTY_TYPE_NUMBER
+  }
+}
 embedded_components {
   id: "sprite"
   type: "sprite"
