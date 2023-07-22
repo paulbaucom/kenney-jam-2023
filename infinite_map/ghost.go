fa57d40e78b00404c132ca148041aed2fae3e12e
Paul Baucom <paul.baucom@outlook.com>
2023-07-22 13:17:36 -0500

    Fix ghost sprite atlas

--- infinite_map/ghost.go
+++ infinite_map/ghost.go
@@ -46,7 +46,7 @@ components {
 embedded_components {
   id: "sprite"
   type: "sprite"
-  data: "tile_set: \"/infinite_map/images/objects.atlas\"\n"
+  data: "tile_set: \"/main/main.atlas\"\n"
   "default_animation: \"ghost_normal\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
